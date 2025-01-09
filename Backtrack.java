f682f43ef21fbd96c6ca4c4d9c7fffd64237d51d
kshitijmadpuwar <kshitijmadpuwar@mail.com>
2025-01-09 15:02:56 +0530

    Code for finding  grid way

--- Backtrack.java
+++ Backtrack.java
@@ -73,7 +73,27 @@ public class Backtrack{
     }
  */
 
-// this variable track the total ways to palce the queen
+
+
+ // Code for Find the number of way's to reach from (0,0 ) to (n-1, m-1) in a N*M Griad.Aloved moves-> Right, Douwn
+     public static int gridways(int i, int j, int n, int m){
+        // BaseCase
+        if(i==n-1 && j==m-1){//condit for last call
+            return 1;
+        }else if(i==n || j==n){ //Boundry cross condition
+            return 0;
+        }
+        
+        //work
+        int w1=gridways(i+1, j, n, m);
+        int w2=gridways(i, j+1, n, m);
+        return w1+w2;
+     }
+
+
+
+     
+ // this variable track the total ways to palce the queen
     static int coutways=0;
     public static void main(String args[]){
         int n=4;
@@ -85,14 +105,17 @@ public class Backtrack{
             }
             System.out.println();
           }
-          if(Nqueen(board, 0)){
+          /*if(Nqueen(board, 0)){
             System.out.println("solution is possible");
             printboard(board);
           }else{
             System.out.println("Solution is not possible");
           }
-
-          System.out.println("Calculate total ways to place's Queen_ " + coutways);
+      */
+       //   System.out.println("Calculate total ways to place's Queen_ " + coutways);
+      int n1=3;
+      int  m=3;
+      System.out.println(gridways(0, 0, n1, m)) ;
 
     }
 }
