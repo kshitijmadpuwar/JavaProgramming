8c2b980d1253ab8986a796469df6561653dd90c4
kshitijmadpuwar <kshitijmadpuwar@mail.com>
2025-01-13 11:09:37 +0530

    SudokuSolver code added successfully

--- Backtrack.java
+++ Backtrack.java
@@ -1,3 +1,5 @@
+
+
 public class Backtrack{
     // code for N-Queen's
 
@@ -88,34 +90,125 @@ public class Backtrack{
         int w1=gridways(i+1, j, n, m);
         int w2=gridways(i, j+1, n, m);
         return w1+w2;
-     }
+      }
+    // this code is used to solve sudoku
+    public static boolean IsSafe(int sudoku[][],int row, int col, int digit){
+      // To check coloum 
+      for(int i=0;i<=8;i++){
+        if(sudoku[i][col]==digit){
+        return false;
+        }
+      }
+
+      //To check at row
+
+      for(int j=0;j<=8;j++){
+        if(sudoku[row][j]==digit){
+          return false;
+        }
+      }
+
+      // To check each Grid of Sudoku & falmula use to calcu. 
+      //startrow=(row/3)x3;
+      //startcol=(col/3)x3;
+
+      int sr=(row/3)*3;
+      int sc=(col/3)*3;
+      //3x3grid
+      for(int i=sr;i<sr+3;i++){
+        for(int j=sc;j<sc+3;j++){
+          if(sudoku[i][j]==digit){
+            return false;
+          }
+        }
+      }
+      return true;
+
+    }
+    
+    public static boolean sudokuSolver(int sudoku[][], int row, int col){
+      //Base case
+      if(row==9 && col==0){
+        return true;
+      }
 
 
+      //Recursion
+      int nextRow=row, nextCol=col+1;
+      if(col+1==9){
+        nextRow=row+1;
+        nextCol=0;
+      }
 
-     
+      if(sudoku[row][col] != 0){
+         return sudokuSolver(sudoku, nextRow, nextCol);
+      }
+
+      for(int digit=1; digit<=9;digit++){
+        if(IsSafe(sudoku,row,col, digit)){
+          sudoku[row][col]= digit;
+          if(sudokuSolver(sudoku, nextRow, nextCol)){
+            return true;
+          }
+          sudoku[row][col]=0;
+        }
+      }
+      return false;
+    }
+
+    public static void printsudoku(int sudoku[][]){
+      for(int i=0;i<9;i++){
+        for(int j=0; j<9; j++){
+          System.out.print(sudoku[i][j]+" ");
+        }System.out.println();
+      }
+
+    }
+  
  // this variable track the total ways to palce the queen
     static int coutways=0;
     public static void main(String args[]){
-        int n=4;
-        char board[][]=new char[n][n];
+      //   int n=4;
+      //   char board[][]=new char[n][n];
 
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board.length;j++){
-              board[i][j]='x';
-            }
-            System.out.println();
-          }
-          /*if(Nqueen(board, 0)){
-            System.out.println("solution is possible");
-            printboard(board);
-          }else{
-            System.out.println("Solution is not possible");
-          }
-      */
-       //   System.out.println("Calculate total ways to place's Queen_ " + coutways);
-      int n1=3;
-      int  m=3;
-      System.out.println(gridways(0, 0, n1, m)) ;
+      //   for(int i=0;i<board.length;i++){
+      //       for(int j=0;j<board.length;j++){
+      //         board[i][j]='x';
+      //       }
+      //       System.out.println();
+      //     }
+      //     /*if(Nqueen(board, 0)){
+      //       System.out.println("solution is possible");
+      //       printboard(board);
+      //     }else{
+      //       System.out.println("Solution is not possible");
+      //     }
+      // */
+      //  //   System.out.println("Calculate total ways to place's Queen_ " + coutways);
+      // int n1=3;
+      // int  m=3;
+      // System.out.println(gridways(0, 0, n1, m)) ;
 
-    }
+      
+      //sudoku
+      int sudoku[][]={{0,0,8,0,0,0,0,0,0},
+      {4,9,0,1,5,7,0,0,2},
+      {0,0,3,0,0,4,1,9,0},
+      {1,8,5,0,6,0,0,2,0},
+      {0,0,0,0,2,0,0,6,0},
+      {9,6,0,4,0,0,3,0,0},
+      {0,3,0,0,7,2,0,0,4},
+      {0,4,9,0,3,0,0,5,7},
+      {8,2,7,0,0,9,0,1,3}
+    };
+
+    
+    if (sudokuSolver(sudoku, 0, 0)) {
+      System.out.println("Solution exists:");
+      printsudoku(sudoku);
+  } else {
+      System.out.println("No solution exists.");
+  }
+  }
 }
+  
